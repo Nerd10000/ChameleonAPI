886ac2285e97ab3dcda955862cf789f9d961662e
Dragon64 <96385928+Nerd10000@users.noreply.github.com>
2025-05-11 14:05:02 +0200

    Added a lot...

--- src/main/java/dragon/me/chameleonAPI/interfaces/ChameleonAPI.java
+++ src/main/java/dragon/me/chameleonAPI/interfaces/ChameleonAPI.java
@@ -1,6 +1,17 @@
 package dragon.me.chameleonAPI.interfaces;
 
+import dragon.me.chameleonAPI.hooks.AdventureCoreHook;
+import dragon.me.chameleonAPI.hooks.AdventureJsonSerializerHook;
+import dragon.me.chameleonAPI.hooks.AdventureMiniMessageHook;
+import dragon.me.chameleonAPI.hooks.SparkHook;
+import dragon.me.chameleonAPI.hooks.packets.PacketEventsHook;
+
 public interface ChameleonAPI {
 
+    PacketEventsHook getPacketEvents();
+    AdventureCoreHook getAdventureHook();
+    AdventureJsonSerializerHook getJsonSerializers();
+    AdventureMiniMessageHook getMiniMessageSerializers();
+    SparkHook getSystemTools();
 
 }
